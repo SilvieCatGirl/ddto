c990336ed9c3bffc9269966660b2b25875c25340
Niko <147955100+SilvieCatGirl@users.noreply.github.com>
2026-08-05 13:54:13 -0400

    natsuki skins added

--- scripts/skins.hx
+++ scripts/skins.hx
@@ -5,6 +5,10 @@ function onCreatePost()
 {
 // thank you ashley for this <3
 	var sayoriSkin:Null<String> = ClientPrefs.equipment.get('sayoriSkin');
+	var natsukiSkin:Null<String> = ClientPrefs.equipment.get('natsukiSkin');
+	var monikaSkin:Null<String> = ClientPrefs.equipment.get('monikaSkin');
+	var yuriSkin:Null<String> = ClientPrefs.equipment.get('yuriSkin');
 	
 	if (PlayState.SONG.player2 == "sayori" && sayoriSkin != null) changeCharacter(sayoriSkin, 1);
+	if (PlayState.SONG.player2 == "natsuki" && natsukiSkin != null) changeCharacter(natsukiSkin, 1);
 }
