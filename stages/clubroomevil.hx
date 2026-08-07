c08952ee9fdf3e18068587705f938819c2948538
Niko <147955100+SilvieCatGirl@users.noreply.github.com>
2026-08-06 21:12:11 -0400

    hmmm

--- stages/clubroomevil.hx
+++ stages/clubroomevil.hx
@@ -53,3 +53,57 @@ function onCreatePost()
 	game.boyfriend.visible = false;
 	camSpecialThing([600, 275], [600, 275], 0.8);
 }
+
+function onBeatHit(){
+				switch (curBeat)
+				{
+					case 1:
+						camZooming = true;
+					case 4:
+						FlxTween.tween(FlxG.camera, {zoom: 0.94}, 2.5, {
+							ease: FlxEase.sineOut,
+							onComplete: function(tween:FlxTween)
+							{
+								defaultCamZoom = 0.9;
+							}
+						});
+					case 72:
+						FlxTween.tween(FlxG.camera, {zoom: 0.8}, 2.5, {
+							ease: FlxEase.sineOut,
+							onComplete: function(tween:FlxTween)
+							{
+								defaultCamZoom = 0.8;
+							}
+						});
+					case 580:
+						FlxTween.tween(FlxG.camera, {zoom: 1}, 2.5, {
+							ease: FlxEase.sineOut,
+							onComplete: function(tween:FlxTween)
+							{
+								defaultCamZoom = 1.1;
+							}
+						});
+					case 648:
+						FlxTween.tween(FlxG.camera, {zoom: 0.9}, 2.5, {
+							ease: FlxEase.sineOut,
+							onComplete: function(tween:FlxTween)
+							{
+								defaultCamZoom = 0.9;
+							}
+						});
+
+						popup.alpha = 1;
+						popup.animation.play('idle', true);
+					case 776:
+						dad.playAnim('lastNOTE_start');
+						dad.specialAnim = true;
+					case 780:
+						dad.playAnim('lastNOTE_end');
+						dad.specialAnim = true;
+					case 788:
+						FlxTween.tween(playHUD.iconP2, {alpha: 0}, 0.25, {ease: FlxEase.sineOut});
+						game.dad.y = 10000;
+					case 790:
+						game.camGame.fade(FlxColor.BLACK, 0.7, false);
+				}
+}
