28e5912a5de5c849ee3ad62801713be10e5d7de6
Niko <147955100+SilvieCatGirl@users.noreply.github.com>
2026-09-06 17:31:25 -0400

    forgot

--- data/notetypes/Hey!.hx
+++ data/notetypes/Hey!.hx
@@ -1,24 +1,23 @@
 function setupNote(note)
 {
-    if (note.noteType == 'Hey!')
-    {
-	if (!PlayState.isPixelStage){
-	note.reloadNote('markov/', 'NOTE_assets');
+    if (note.noteType == 'Hey!'){
+		if (!PlayState.isPixelStage){
+			note.reloadNote('markov/', 'NOTE_assets');
+		}
+		if (PlayState.isPixelStage){
+			note.setCustomColor([0xFF101010, 0xFFFF0000, 0xFF990022]);
+		}
+		note.noAnimation = true;
+		if (PlayState.SONG.song == "Obsession"){
+			note.noAnimation = false;
+		}
+		note.ignoreNote = false;
+		note.canMiss = true;
+		note.hitPriority = 0;
+		note.rgbEnabled = false;
+		note.hitCausesMiss = true;
+		note.missHealth = 5;
 	}
-	if (PlayState.isPixelStage){
-	note.setCustomColor([0xFF101010, 0xFFFF0000, 0xFF990022]);
-	}
-        note.noAnimation = true;
-	if (PlayState.SONG.song == "Obsession"){
-        note.noAnimation = false;
-	}
-        note.ignoreNote = false;
-        note.canMiss = true;
-        note.hitPriority = 0;
-	note.rgbEnabled = false;
-        note.hitCausesMiss = true;
-        note.missHealth = 5;
-    }
 }
 
 function postSpawnNote(note)
